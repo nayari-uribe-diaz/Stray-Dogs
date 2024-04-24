c714bad1a3cfa902451fd2eeba37d8fadf318651
Nayari Uribe Diaz <nayariu7@gmail.com>
2024-04-24 13:57:27 -0700

    add css and edits

--- webapp.py
+++ webapp.py
@@ -11,6 +11,38 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('about.html')
+    
+@app.route("/causes")
+def render_causes():
+    return render_template('causes.html')
+    
+@app.route("/health")
+def render_health():
+    return render_template('healthImpacts.html')
+    
+@app.route("/attacks")
+def render_attacks():
+    return render_template('attacks.html')
+    
+@app.route("/gov")
+def render_gov():
+    return render_template('government.html')
+    
+@app.route("/solutions")
+def render_solutions():
+    return render_template('solutions.html')
+    
+        
+def is_localhost():
+    """ Determines if app is running on localhost or not
+    Adapted from: https://stackoverflow.com/questions/17077863/how-to-see-if-a-flask-app-is-being-run-on-localhost
+    """
+    root_url = request.url_root
+    developer_url = 'http://127.0.0.1:5000/'
+    return root_url == developer_url
     
     
+
+if __name__=="__main__":
+    app.run(debug=False)
